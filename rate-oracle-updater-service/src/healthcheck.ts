3a8d4370447e3e9f1b566d56276de9970ace354d
Nazar Duchak <nazar@iovlabs.org>
2020-12-04 18:20:16 +0200

    feat: add heathcheck for node connection

--- rate-oracle-updater-service/src/healthcheck.ts
+++ rate-oracle-updater-service/src/healthcheck.ts
@@ -1,19 +1,15 @@
+import Eth from 'web3-eth'
 import type { Application } from './definitions'
 
 const HEALTHCHECK_ROUTE = '/healthcheck'
 export default function (app: Application): void {
   app.use(HEALTHCHECK_ROUTE, async (req, res) => {
-    // try {
-    //   await storage.version()
-    // } catch (e) {
-    //   res.status(500).send('No Storage connection')
-    // }
-    //
-    // try {
-    //   await sequelize.authenticate()
-    // } catch (e) {
-    //   res.status(500).send('No DB connection')
-    // }
+    const eth = app.get('eth') as Eth
+    try {
+      await eth.getProtocolVersion()
+    } catch (e) {
+      res.status(500).send('No blockchain node connection')
+    }
 
     res.status(204).end()
   })
